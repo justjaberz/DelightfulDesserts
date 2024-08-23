51824dee8b60053d800a288e8b5e4374ce4a6f06
justjaberz <jaceyalder@gmail.com>
2024-08-22 22:58:42 -0500

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -6,7 +6,9 @@ document.getElementById('contact-form').addEventListener('submit', function(even
   var subject = document.getElementById('subject').value;
   var message = document.getElementById('message').value;
 
-  var mailtoLink = `mailto:jaceyalder@gmail.com?subject=${encodeURIComponent(subject)}&body=Name: ${encodeURIComponent(name)}%0AEmail: ${encodeURIComponent(email)}%0A%0A${encodeURIComponent(message)}`;
+  var mailtoLink = `mailto:delightfuldesserts@business.com?subject=${encodeURIComponent(subject)}&body=Name: ${encodeURIComponent(name)}%0AEmail: ${encodeURIComponent(email)}%0A%0A${encodeURIComponent(message)}`;
   
   window.location.href = mailtoLink;
 });
+
+
