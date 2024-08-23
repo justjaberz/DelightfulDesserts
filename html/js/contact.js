7ec19b74784cebe5d81d2ff5bc4173976ef8af27
justjaberz <102921533+justjaberz@users.noreply.github.com>
2024-08-22 21:51:01 -0500

    Update contact.js

--- html/js/contact.js
+++ html/js/contact.js
@@ -1,12 +1,12 @@
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var subject = document.getElementById('subject').value;
-    var message = document.getElementById('message').value;
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var subject = document.getElementById('subject').value;
+  var message = document.getElementById('message').value;
 
-    var mailtoLink = `mailto:your-email@example.com?subject=${encodeURIComponent(subject)}&body=Name: ${encodeURIComponent(name)}%0AEmail: ${encodeURIComponent(email)}%0A%0A${encodeURIComponent(message)}`;
-    
-    window.location.href = mailtoLink;
-  });
+  var mailtoLink = `mailto:your-email@example.com?subject=${encodeURIComponent(subject)}&body=Name: ${encodeURIComponent(name)}%0AEmail: ${encodeURIComponent(email)}%0A%0A${encodeURIComponent(message)}`;
+  
+  window.location.href = mailtoLink;
+});
